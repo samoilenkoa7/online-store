394123a8d48aca4734d72aa760c60c18b3f745cc
samoilenkoa7 <samoilenkoa7@gmail.com>
2022-09-26 00:14:43 -0400

    last updates

--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from pathlib import Path
 import os
-from .secret import *
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
+SECRET_KEY = config('secret_key', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -211,4 +212,4 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-TOKEN_BOT = telegramm_token
+TOKEN_BOT = config('telegramm_token', default='')
