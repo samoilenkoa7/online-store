5ade5bbac8d591d14188322d5807cd5f879b2911
samoilenkoa7 <samoilenkoa7@gmail.com>
2022-09-26 21:37:18 -0400

    database changed to postgresql

--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'mySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fort-shop',
+        'USER': 'postgres',
+        'PASSWORD': 'вфеф4323',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
